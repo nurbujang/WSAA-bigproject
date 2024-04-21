08563220cade49ac3e16d5e2ba20325e735f5e61
rsalgo <tinisufina@gmail.com>
2024-04-21 06:39:27 +0100

    put hash not '''

--- flask.py
+++ flask.py
@@ -1,39 +1,35 @@
-'''
-Project for Web Services and Applications (Spring 2024)
-flask.py
-Author: Nur Bujang
+# Project for Web Services and Applications (Spring 2024)
+# flask.py
+# Author: Nur Bujang
 
-Create a Web application in Flask that has a RESTful API.
-The application should link to one or more database tables.
-You should also create the web pages that can consume the API. 
-I.e. performs CRUD operations on the data.
+# Create a Web application in Flask that has a RESTful API.
+# The application should link to one or more database tables.
+# You should also create the web pages that can consume the API. 
+# I.e. performs CRUD operations on the data.
 
-Description:
-A rehash of the sample project lab 
-i.e. A basic Flask server that has a:
-    1. REST API, (to perform CRUD operations)
-    2. One database table and
-    3. Accompanying web interface, using AJAX calls, to perform these CRUD operations
+# Description:
+# A rehash of the sample project lab 
+# i.e. A basic Flask server that has a:
+#     1. REST API, (to perform CRUD operations)
+#     2. One database table and
+#     3. Accompanying web interface, using AJAX calls, to perform these CRUD operations
 
-▪ Make it look nice
-▪ Have more than one database table
-▪ Link to an outside data source and either:
-    • Save the data into a database table that you view elsewhere in your application.
-    • Manipulate/analyse the data in real time.
-▪ Hosted your application online (e.g. Azure, Pythonanywhere).
-    • Be warned that some free hosting does not allow access to third party systems
-▪ Perform more than CRUD operations on your data
+# ▪ Make it look nice
+# ▪ Have more than one database table
+# ▪ Link to an outside data source and either:
+#     • Save the data into a database table that you view elsewhere in your application.
+#     • Manipulate/analyse the data in real time.
+# ▪ Hosted your application online (e.g. Azure, Pythonanywhere).
+#     • Be warned that some free hosting does not allow access to third party systems
+# ▪ Perform more than CRUD operations on your data
 
-'''
 
 # 
 
 
 
-'''
-REFERENCES
+# REFERENCES
 
 
-_______________________________________________________________________________________________
-End
-'''
+# _______________________________________________________________________________________________
+# End
