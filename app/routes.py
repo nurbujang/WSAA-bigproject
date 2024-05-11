65a78a680720c19aa8eb34f80d49116a71746b0f
rsalgo <tinisufina@gmail.com>
2024-05-11 16:14:27 +0100

    add mysql in routes

--- app/routes.py
+++ app/routes.py
@@ -50,3 +50,15 @@ def dashboard():
         print('GET??')
 
     return render_template('dashboard.html', title='Dashboard')
+
+# create table login (username varchar(250) NOT NULL, 
+# password varchar(250) NOT NULL, 
+# priviledge text);
+# insert INTO login (username, password, priviledge) values ("admin", "password", "admin");
+# select * from login;
+
+# create table airport (country varchar(250) NOT NULL, 
+# year (int), 
+# main varchar(250),
+# other varchar(250),
+# small varchar(250)
