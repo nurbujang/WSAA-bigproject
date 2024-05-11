6c398fc74bdb20f3ca53d24a4cd9ad253d371e8b
rsalgo <tinisufina@gmail.com>
2024-05-11 11:27:28 +0100

    gitinore update

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+
+"""
+git config --global user.name "Your Name"
+git config --global user.email "you@example.com"
+what name do you use for git and email? nurbujang, tinisufina@gmail.com
+u want to clone?
+
+"""
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+#/home/nurbujang/.virtualenvs/my_flask
 
 @app.route("/")
 @app.route("/index")
